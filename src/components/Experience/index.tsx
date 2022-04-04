79d8081155c4b1971ffe1e168898decc11a33412
Gie-sta <70339208+Gie-sta@users.noreply.github.com>
2022-04-04 16:37:37 +0300

    work examples created & styles added

---
+++ src/components/Experience/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "./styles.module.scss";
+
+type Props = {};
+
+const Expierence = (props: Props) => {
+  return <div>Expierence</div>;
+};
+
+export default Expierence;
